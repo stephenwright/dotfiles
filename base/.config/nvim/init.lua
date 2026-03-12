bac0deb011121e7e30a9e5a70b1404eeba10b653
Stephen Wright <stephen@binsentry.com>
2026-03-12 15:49:39 -0400

    which-key

--- base/.config/nvim/init.lua
+++ base/.config/nvim/init.lua
@@ -1,5 +1,3 @@
----@diagnostic disable: undefined-global
-
 -- leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -15,11 +13,18 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.scrolloff = 3
 
 vim.opt.wrap = false
 vim.opt.colorcolumn = '80'
+
+vim.opt.undofile = true
+vim.opt.signcolumn = 'yes'
+vim.opt.termguicolors = true
+vim.opt.updatetime = 250
 
 -- Toggle word wrap with <leader>w
 vim.keymap.set("n", "<leader>w", function()
@@ -32,29 +37,32 @@ vim.keymap.set("n", "<leader>i", function()
   vim.lsp.buf.format({ async = true })
 end, { desc = "Format current buffer" })
 
+-- diagnostics
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Show diagnostic" })
+vim.keymap.set('n', '<leader>q', function() require('fzf-lua').diagnostics_document() end, { desc = "List diagnostics" })
+
 -- clipboard operations
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
-vim.keymap.set({'n', 'v'}, '<leader>yy', '"+yy')
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+p')
-vim.keymap.set({'n', 'v'}, '<leader>P', '"+P')
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = "Yank to clipboard" })
+vim.keymap.set({'n', 'v'}, '<leader>yy', '"+yy', { desc = "Yank line to clipboard" })
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set({'n', 'v'}, '<leader>P', '"+P', { desc = "Paste before from clipboard" })
 
 -- plugins
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then ---@diagnostic disable-line: undefined-field
   vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    '--branch=stable',
     lazypath,
   })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
--- plugins
 require('lazy').setup({
 
   -- themes
@@ -72,9 +80,7 @@ require('lazy').setup({
       require('rose-pine').setup({
         disable_italics = true,
       })
-      -- Try rose-pine first, fallback to catppuccin
-      local ok, _ = pcall(vim.cmd, 'colorscheme rose-pine')
-      if not ok then
+      if not pcall(function() vim.cmd('colorscheme rose-pine') end) then
         vim.cmd('colorscheme catppuccin')
       end
     end,
@@ -105,15 +111,29 @@ require('lazy').setup({
     end,
   },
 
+  -- keybinding help
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    config = function()
+      local wk = require('which-key')
+      wk.setup({ preset = 'helix' })
+      wk.add({
+        { '<leader>h', group = 'git hunks' },
+        { '<leader>y', group = 'clipboard' },
+      })
+    end,
+  },
+
   -- fuzzy finder
   {
     'ibhagwan/fzf-lua',
     name = 'fzf',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
-      { '<leader>f', function() require('fzf-lua').files() end, mode = 'n', silent = true },
-      { '<leader>b', function() require('fzf-lua').buffers() end, mode = 'n', silent = true },
-      { '<leader>/', function() require('fzf-lua').grep_project() end, mode = 'n', silent = true },
+      { '<leader>f', function() require('fzf-lua').files() end, desc = "Find files" },
+      { '<leader>b', function() require('fzf-lua').buffers() end, desc = "Find buffers" },
+      { '<leader>/', function() require('fzf-lua').grep_project() end, desc = "Grep project" },
     },
   },
 
@@ -141,12 +161,12 @@ require('lazy').setup({
         on_attach = function(bufnr)
           local gs = package.loaded.gitsigns
 
-          vim.keymap.set('n', ']c', gs.next_hunk, {buffer = bufnr})
-          vim.keymap.set('n', '[c', gs.prev_hunk, {buffer = bufnr})
-          vim.keymap.set('n', '<leader>hs', gs.stage_hunk, {buffer = bufnr})
-          vim.keymap.set('n', '<leader>hr', gs.reset_hunk, {buffer = bufnr})
-          vim.keymap.set('n', '<leader>hp', gs.preview_hunk, {buffer = bufnr})
-          vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end, {buffer = bufnr})
+          vim.keymap.set('n', ']c', gs.next_hunk, {buffer = bufnr, desc = "Next hunk"})
+          vim.keymap.set('n', '[c', gs.prev_hunk, {buffer = bufnr, desc = "Prev hunk"})
+          vim.keymap.set('n', '<leader>hs', gs.stage_hunk, {buffer = bufnr, desc = "Stage hunk"})
+          vim.keymap.set('n', '<leader>hr', gs.reset_hunk, {buffer = bufnr, desc = "Reset hunk"})
+          vim.keymap.set('n', '<leader>hp', gs.preview_hunk, {buffer = bufnr, desc = "Preview hunk"})
+          vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end, {buffer = bufnr, desc = "Blame line"})
         end
       })
     end,
@@ -159,8 +179,12 @@ require('lazy').setup({
     build = ':TSUpdate',
     event = 'BufReadPre',
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require'nvim-treesitter.configs'.setup {
-        ensure_installed = { 'javascript', 'typescript', 'lua', 'vim', 'vimdoc', 'query' },
+        ensure_installed = {
+          'javascript', 'typescript', 'lua', 'vim', 'vimdoc', 'query',
+          'go', 'python', 'html', 'css', 'json', 'yaml', 'bash', 'zig',
+        },
         sync_install = false,
         auto_install = true,
         highlight = {
@@ -171,28 +195,22 @@ require('lazy').setup({
     end,
   },
 
+  -- neovim lua development
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {},
+  },
+
+  -- lsp
+  {
+    'neovim/nvim-lspconfig',
     event = 'BufReadPre',
     dependencies = {
-      -- lsp
-      'neovim/nvim-lspconfig',
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-      -- autocomplete
-      'hrsh7th/nvim-cmp',
-      'hrsh7th/cmp-nvim-lsp',
-      -- snippets
-      'L3MON4D3/LuaSnip',
     },
     config = function()
-      local lsp_zero = require('lsp-zero')
-
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
-
       require('mason').setup({})
       require('mason-lspconfig').setup({
         ensure_installed = {
@@ -206,24 +224,28 @@ require('lazy').setup({
           'zls',
         },
         handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lspconfig = require('lspconfig')
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            lua_opts.settings.Lua.diagnostics.globals = { 'vim' }
-            lspconfig.lua_ls.setup(lua_opts)
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
           end,
         }
       })
-
-      local cmp = require('cmp')
-      cmp.setup({
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        },
-      })
     end,
   },
-})
 
+  -- autocomplete
+  {
+    'saghen/blink.cmp',
+    version = '1.*',
+    event = 'InsertEnter',
+    dependencies = {
+      'L3MON4D3/LuaSnip',
+    },
+    opts = {
+      keymap = { preset = 'default' },
+      sources = {
+        default = { 'lsp', 'snippets', 'path', 'buffer' },
+      },
+      snippets = { preset = 'luasnip' },
+    },
+  },
+})
